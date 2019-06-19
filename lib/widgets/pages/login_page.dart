91b03487636508a2b8e0b76cbe2f28a7722e969c
ChangJoo Park <pcjpcj2@gmail.com>
2019-06-20 05:13:01 +0900

    Implement login_page

--- lib/widgets/pages/login_page.dart
+++ lib/widgets/pages/login_page.dart
@@ -1,29 +1,97 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _validate = false;
+  String _email;
+  String _password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('로그인'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text(
-              '로그인 페이지',
-              style: TextStyle(fontSize: 50),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Form(
+                key: _formKey,
+                autovalidate: _validate,
+                child: loginFormWidget(),
+              ),
             ),
-            RaisedButton(
-              child: Text('Login'),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/');
-              },
-            ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  Widget loginFormWidget() {
+    final _passwordField = TextFormField(
+      decoration: const InputDecoration(labelText: '비밀번호'),
+      obscureText: true,
+      keyboardType: TextInputType.text,
+      validator: validatePassword,
+      onSaved: (String val) {
+        _password = val;
+      },
+    );
+
+    final _emailField = TextFormField(
+      decoration: const InputDecoration(labelText: '이메일'),
+      keyboardType: TextInputType.emailAddress,
+      validator: validateEmail,
+      onSaved: (String val) {
+        _email = val;
+      },
+    );
+
+    return Column(
+      children: <Widget>[
+        _emailField,
+        _passwordField,
+        SizedBox(height: 10.0),
+        RaisedButton(onPressed: _submit, child: Text('로그인'))
+      ],
+    );
+  }
+
+  _submit() {
+    if (_formKey.currentState.validate()) {
+      // No any error in validation
+      _formKey.currentState.save();
+      print("Name $_name");
+      print("Email $_email");
+    } else {
+      // validation error
+      setState(() {
+        _validate = true;
+      });
+    }
+  }
+
+  String validatePassword(String value) {
+    if (value.length < 6)
+      return 'Password must be more than 2 charater';
+    else
+      return null;
+  }
+
+  String validateEmail(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return 'Enter Valid Email';
+    else
+      return null;
   }
 }
