dcf114783d3f7c6bf937bdfc6bbb5664aee7980c
ChangJoo Park <pcjpcj2@gmail.com>
2019-06-20 04:24:33 +0900

    Update default routes

---
+++ lib/widgets/pages/first_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Routing App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'First Page',
+              style: TextStyle(fontSize: 50),
+            ),
+            RaisedButton(
+              child: Text('Go to second'),
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  '/second',
+                  arguments: 'Hello there from the first page!',
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/pages/second_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  // This is a String for the sake of an example.
+  // You can use any type you want.
+  final String data;
+
+  SecondPage({
+    Key key,
+    @required this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Routing App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'Second Page',
+              style: TextStyle(fontSize: 50),
+            ),
+            Text(
+              data,
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
