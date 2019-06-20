b12ee833fa662556538fc18e27856591f058a194
ChangJoo Park <pcjpcj2@gmail.com>
2019-06-21 02:00:07 +0900

    extract drawer from HomePage

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+Drawer createDrawer(BuildContext context) {
+  return Drawer(
+    // Add a ListView to the drawer. This ensures the user can scroll
+    // through the options in the drawer if there isn't enough vertical
+    // space to fit everything.
+    child: ListView(
+      // Important: Remove any padding from the ListView.
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+        DrawerHeader(
+          child: Text('Drawer Header'),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+        ),
+        ListTile(
+          title: Text('Item 1'),
+          onTap: () {
+            Navigator.of(context).pop();
+            Navigator.of(context).pushNamed(
+              '/second',
+              arguments: 'Hello there from the first page!',
+            );
+          },
+        ),
+        ListTile(
+          title: Text('Item 2'),
+          onTap: () {
+            // Update the state of the app.
+            // ...
+          },
+        ),
+      ],
+    ),
+  );
+}
