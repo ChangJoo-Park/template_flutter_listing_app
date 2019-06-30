b1f3d1a78b9b2726bddd1b07b67548ca008da299
ChangJoo Park <pcjpcj2@gmail.com>
2019-06-30 18:23:42 +0900

    Implement Drawer when login and logged out

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,8 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../states/account_state.dart';
 
 /// createDrawer need [context] for navigate other pages.
 /// Must need [context]
 Drawer createDrawer(BuildContext context) {
+  final $account = Provider.of<AccountState>(context);
+  return $account.loggedIn
+      ? createUserAccountDrawer(context, $account)
+      : createGuestDrawer(context);
+}
+
+Drawer createUserAccountDrawer(BuildContext context, AccountState account) {
+  return Drawer(
+    child: ListView(
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+        UserAccountsDrawerHeader(
+          accountEmail: Text("pcjpcj2@gmail.com"),
+          accountName: Text("ChangJoo Park"),
+          currentAccountPicture: CircleAvatar(
+            backgroundColor: Colors.white,
+            backgroundImage: NetworkImage(
+              "https://images.clipartlogo.com/files/images/32/325521/mushroom-vector-power_f.png",
+            ),
+          ),
+        ),
+        ListTile(
+          title: Text('Home'),
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        ListTile(
+          title: Text('Second Page'),
+          onTap: () {
+            Navigator.of(context).pop();
+            Navigator.of(context)
+                .pushNamed('/second', arguments: "Hello there, from HomePage");
+          },
+        ),
+        ListTile(
+          title: Text('Logout'),
+          onTap: () {
+            account.logout();
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    ),
+  );
+}
+
+Drawer createGuestDrawer(BuildContext context) {
   return Drawer(
     // Add a ListView to the drawer. This ensures the user can scroll
     // through the options in the drawer if there isn't enough vertical
@@ -12,26 +62,19 @@ Drawer createDrawer(BuildContext context) {
       padding: EdgeInsets.zero,
       children: <Widget>[
         DrawerHeader(
-          child: Text('Drawer Header'),
+          child: Text(
+            'Please Sign In',
+            style: TextStyle(fontSize: 20),
+          ),
           decoration: BoxDecoration(
             color: Colors.blue,
           ),
         ),
         ListTile(
-          title: Text('Item 1'),
+          title: Text('Login'),
           onTap: () {
             Navigator.of(context).pop();
-            Navigator.of(context).pushNamed(
-              '/second',
-              arguments: 'Hello there from the first page!',
-            );
-          },
-        ),
-        ListTile(
-          title: Text('Item 2'),
-          onTap: () {
-            // Update the state of the app.
-            // ...
+            Navigator.of(context).pushReplacementNamed('/login');
           },
         ),
       ],
